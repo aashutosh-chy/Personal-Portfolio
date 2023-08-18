6869206e1e4f1afabd73c559fbbf61931427cd59
Aashutosh Chaudhary <64635204+aashutosh-chy@users.noreply.github.com>
2023-08-18 12:39:53 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("user_YG-SNVazrXLyORzLMjJAK");
 
         emailjs.sendForm('service_uerxitf', 'template_rt2lfuv', '#contact-form')
             .then(function (response) {
